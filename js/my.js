b9ac4d4773874e78c05f84f082c4a159b208cb35
Hudson590 <52305767+hudson5901@users.noreply.github.com>
2020-08-03 10:20:37 +0900

    first commit

---
+++ js/my.js
@@ -0,0 +1,10 @@
+ $(".button-collapse").sideNav();
+
+
+ $(".tab_label").on("click",function(){
+    var $th = $(this).index();
+    $(".tab_label").removeClass("active");
+    $(".tab_panel").removeClass("active");
+    $(this).addClass("active");
+    $(".tab_panel").eq($th).addClass("active");
+   });
