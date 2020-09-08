e2185c9e65d65f6651a76f647d92903d10d6d31f
hudson5901 <rinng.nakamura@gmail.com>
2020-09-08 16:30:23 +0900

    fast commit

--- js/my.js
+++ js/my.js
@@ -1,10 +1,27 @@
- $(".button-collapse").sideNav();
+$(".button-collapse").sideNav();
 
 
- $(".tab_label").on("click",function(){
-    var $th = $(this).index();
-    $(".tab_label").removeClass("active");
-    $(".tab_panel").removeClass("active");
-    $(this).addClass("active");
-    $(".tab_panel").eq($th).addClass("active");
+$(".tab_label").on("click", function () {
+   var $th = $(this).index();
+   $(".tab_label").removeClass("active");
+   $(".tab_panel").removeClass("active");
+   $(this).addClass("active");
+   $(".tab_panel").eq($th).addClass("active");
+});
+
+$(function () {
+   $('.tab_btn').on('click', function () {
+      $('.tab_item').removeClass("is-active-item");
+      $($(this).attr("href")).addClass("is-active-item");
+      $('.tab_btn').removeClass('is-active-btn');
+      $(this).addClass('is-active-btn');
    });
+});
+
+
+$(function () {
+   // 「#change-css」要素に対するclickイベントを作成してください
+   $("#change-css").on('click', function () {
+      $("#hogehoge").html("要素の内容を書き換えます");
+   });
+});
